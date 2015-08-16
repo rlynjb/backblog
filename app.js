f767cc4376d10282d1789bca77c04f0c41d73673
rlynjb <rlynjb@gmail.com>
2015-08-15 17:13:38 -0700

    clean up code a lil

--- app.js
+++ app.js
@@ -41,12 +41,17 @@ $(function() {
       this.collection.fetch({
         success: function(a, b, c) {
           for (var k in b) {
+            // get instance of model
             var post = new Post({
               title: b[k]['title'],
               body: b[k]['body']
             });
 
+            // get instance of view and assign model
             var postView = new PostView({ model: post });
+
+            // append in template
+            // this is a hack
             $('#main').append( postView.el );
           }
         },
@@ -69,42 +74,7 @@ $(function() {
       this.$el.html( this.template(this.model.toJSON()) );
     }
   });
-
   var postview = new PostView();
-  //console.log(postView);
 
-  /*
-  var PostListView = Backbone.View.extend({
-    tagName: "li",
-    template: _.template("<a href='/posts/<%= id %>'><%= title %></a>"),
-    initialize: function() {
-      this.render();
-    },
-    render: function() {
-      this.$el.html( this.template(this.model.toJSON()) );
-    }
-  });
-
-  var PostsListView = Backbone.View.extend({
-    tagName: 'ul',
-    template: _.template("<h1>My Blog</h1><ul></ul>"),
-    render: function () {
-      this.el.innerHTML = this.template();
-      var ul = this.$el.find("ul");
-      this.collection.forEach(function (post) {
-        ul.append(new PostListView({
-          model: post
-        }).render().el);
-      });
-      return this;
-    }
-  });
-
-  var posts = new Posts();
-  $("#main").append(new PostsListView({
-    collection: posts
-  }).render().el);
-  */
-  //var newsItems = new PostsListView({ el: $("#main") });
 
 });
