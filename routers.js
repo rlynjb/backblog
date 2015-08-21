72cf22aa79eac316248e96ca00242ea734b6045c
RLyn Ben <rlyn@brydansolutions.com>
2015-08-21 10:15:52 -0700

    separate viws and routers

---
+++ routers.js
@@ -0,0 +1,51 @@
+// NOTE:
+// Transfer code from showPost router here
+// its best practice for router and to avoid clutter
+// WAS transferring code but came across an Event issue with listenTo
+var blogController = {
+  showBlogPost: function(id, e) {
+    var blogPost = new Post({id:id});
+    blogPost.fetch();
+    e.listenTo( blogPost, 'sync', function() {
+      e.view = new PostPage({ model: blogPost});
+    });     
+  }
+}
+
+// --------------------------------------------------------------
+
+var BlogRouter = Backbone.Router.extend({
+  routes: {
+    "": "showHome",
+    "about": "showAbout",
+    "post/:id": "showPost",
+    "new": "newPost"
+  },
+  showHome: function() {
+    this.view = new Home();
+
+    // Best practice to set data on instance so view can be reusable
+    var postsview = new PostsView({
+      collection: new Posts(),
+      el: '#main'
+    });  
+  },
+  showAbout: function() {
+    this.view = new About();
+  },
+  showPost: function(id) {
+    // Best pratice:
+    // Avoid this chunk of functional code in router
+    // It is best to keep code minimal so it doesnt clutter router
+    // *Also avoid instantiating inside router
+    // Instead, create a Controller and call controller method from router
+    var e = this;
+    blogController.showBlogPost(id, e);
+  },
+  newPost: function() {
+    this.view = new NewPost();
+  }
+});
+var blogrouter = new BlogRouter();
+// Start Backbone history a necessary step for bookmarkable URL's
+Backbone.history.start();
