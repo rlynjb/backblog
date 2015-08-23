d6558bd1ca6c63efea080e3f1418a5e638313805
rlynjb <rlynjb@gmail.com>
2015-08-22 21:43:00 -0700

    task done for week1

--- app/collections.js
+++ app/collections.js
@@ -1,12 +1,30 @@
 var PostsCollection = Backbone.Collection.extend({
   model: PostModel,
-  url: localserver + "/posts",
+  url: localserver + "/posts?_sort=views&_order=DESC",
   parse: function(response) {
     return response;
   }
 });
 
+
 var CommentsCollection = Backbone.Collection.extend({
-  model: CommentModel,
-  url: localserver + '/comments'
+  initialize: function(models, options) {
+    /*
+     * TODO:
+     * do research on this, initialize/contructor method
+     * I heard, Collection initialize handles its arguments different
+     * from Model and Views
+     * https://github.com/jashkenas/backbone/issues/661
+     * */
+    this.id = options.id;
+    console.log(options);
+    console.log(options.post.id);
+
+    return this;
+  },
+  url: function() {
+    console.log('id: ' + this.id);
+    return localserver + "/posts/" + "3" + "/comments";
+  },
+  model: CommentModel
 });
--- app/models.js
+++ app/models.js
@@ -4,6 +4,14 @@
 * Collection and Model automatically assigns primary/unique ID
 * */
 var PostModel = Backbone.Model.extend({
+  initialize: function() {
+    /*
+     * we are connecting Comments collection 
+     * to each post item by passing along post id
+     * */
+    //this.comments = new CommentsCollection([], this.id);
+    this.comments = new CommentsCollection([], { post:this });
+  },
   defaults: {
     title: 'title here',
     body: 'body here'
